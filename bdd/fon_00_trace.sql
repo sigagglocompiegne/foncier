9501a0fbe3e63b5e269670634b7d140816e2a9dc
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-02-10 08:10:16 +0100

    Update fon_00_trace.sql

--- bdd/fon_00_trace.sql
+++ bdd/fon_00_trace.sql
@@ -1,1 +1,13 @@
-A rédiger
+/*Amt_fon_eco (afe) V1.0*/
+/*Creation du fichier trace qui permet de suivre l'évolution du code*/
+/* afe_00_trace.sql */
+/*PostGIS*/
+
+/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet */
+
+/*
+#################################################################### SUIVI CODE SQL ####################################################################
+-- Le développement originel a débuté en 2015. Le développement a évolué et a été optimisé depuis sans qu'il n'y est de documentation.
+-- Début 2023, nouveau modèle Activité Economique incluant les données des opérations d'aménagements et du foncier (suivi des cessions)
+-- 2023-02-10 : GB / initialisation du code uniquement pour la partie acquisitions foncières (la partie cession étant dans le répertoire acti_eco)
